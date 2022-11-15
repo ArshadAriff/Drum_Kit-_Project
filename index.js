760040a4c6bf7fd518691efc3f665a98d06b5a06
ArshadAriff <113685884+ArshadAriff@users.noreply.github.com>
2022-11-15 09:36:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function makeSound(key) {
       crash.play();
       break;
     case "j":
-      var kick = new Audio("kick-bass.mp3");
+      var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
     default:
